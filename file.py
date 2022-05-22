a8e2f136148cff8ea0e9a4a77dbfd72a2ce64c7c
Marsh1894 <dawid.patola1@gmail.com>
2022-05-22 14:16:42 +0200

    make it better

--- file.py
+++ file.py
@@ -1,9 +1,27 @@
 def greeter(func):
-    pass
+    def aloha(*args):
+        result = 'Aloha ' + func(*args)
+        return result.title()
+    return aloha
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def calc(self):
+        values = func(self)
+        values = values.split()
+        for i, j in enumerate(values):
+            if "-" in j:
+                values[i] = [-int(x) for x in j[1:]]
+            else:
+                values[i] = [int(x) for x in j[:]]
+        summary_1 = sum(values[0])
+        summary_2 = sum(values[1])
+
+        if summary_1 == summary_2:
+            return print(f'{summary_1} == {summary_2}')
+        else:
+            return print(f'{summary_1} != {summary_2}')
+    return calc
 
 
 def format_output(*required_keys):
@@ -12,3 +30,4 @@ def format_output(*required_keys):
 
 def add_method_to_instance(klass):
     pass
+
